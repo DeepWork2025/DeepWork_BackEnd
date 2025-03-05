7e86c5f38ca7d16957d60038cea798d2c4ac08ac
EleanorWho <huj1@tcd.ie>
2025-03-05 23:30:09 +0000

    update settings

--- Django/settings.py
+++ Django/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("ALLLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
